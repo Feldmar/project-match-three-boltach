a3012816cf95a76db84d560a1ff020c797379535
Feldmar <89993722+Feldmar@users.noreply.github.com>
2022-04-20 23:29:12 +0300

    create btn with game menu

---
+++ js/menu.js
@@ -0,0 +1,42 @@
+'use strict';
+let gameMenu = function() {
+  let div = document.getElementsByTagName('div')[0];
+
+  let newGameBTN = document.createElement('button');
+  div.appendChild(newGameBTN);
+  newGameBTN.innerHTML = 'New Game';
+  newGameBTN.addEventListener ('click', function() {
+    alert('newGameBTN');
+  });
+
+  let recordsTableBTN = document.createElement('button');
+  div.appendChild(recordsTableBTN);
+  recordsTableBTN.innerHTML = 'TOP';
+  recordsTableBTN.addEventListener ('click', function() {
+    alert('recordsTableBTN');
+  });
+
+  let manualBTN = document.createElement('button');
+  div.appendChild(manualBTN);
+  manualBTN.innerHTML = 'Manual';
+  manualBTN.addEventListener ('click', function() {
+    alert('manualBTN');
+  });
+
+  let aboutBTN = document.createElement('button');
+  div.appendChild(aboutBTN);
+  aboutBTN.innerHTML = 'About';
+  aboutBTN.addEventListener ('click', function() {
+    alert('aboutBTN');
+  });
+
+  let exitBTN = document.createElement('button');
+  div.appendChild(exitBTN);
+  exitBTN.innerHTML = 'Exit Game';
+  exitBTN.addEventListener ('click', function() {
+    alert('exitBTN');
+  });
+
+
+};
+document.addEventListener('DOMContentLoaded', gameMenu);
--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 'use strict';
 let replaceBackground = function() {
-  document.body.style.backgroundImage = 'url(svg/background.svg)';
+  document.body.style.backgroundImage = 'url(../svg/background.svg)';
   document.body.style.backgroundRepeat = 'no-repeat';
   document.body.style.backgroundSize = '100%';
 };
 document.addEventListener('DOMContentLoaded', replaceBackground);
+
+
