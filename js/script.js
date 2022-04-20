8b61d376602e8a0d54f398f3f62a2e0815714282
Feldmar <89993722+Feldmar@users.noreply.github.com>
2022-04-20 22:49:32 +0300

    create background

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-  
+'use strict';
+let replaceBackground = function() {
+  document.body.style.backgroundImage = 'url(svg/background.svg)';
+  document.body.style.backgroundRepeat = 'no-repeat';
+  document.body.style.backgroundSize = '100%';
+};
+document.addEventListener('DOMContentLoaded', replaceBackground);
